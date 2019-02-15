841380bd4907f0c94e360cd77bb950b7fdcdfeee
Julian Bennecker <jb@mainformat.de>
2019-02-15 15:11:35 +0100

    Move CMS Fields to Blog Tab

--- src/BlogPage.php
+++ src/BlogPage.php
@@ -27,10 +27,7 @@ class BlogPage extends Page
         $fields = parent::getCMSFields();
 
         $config = GridFieldConfig_RecordEditor::create();
-        $fields->addFieldToTab(
-            'Root.Categories',
-            GridField::create('BlogCategories', 'BlogCategories', $this->BlogCategories(), $config)
-        );
+        $fields->addFieldToTab('Root.Blog', GridField::create('BlogCategories', 'BlogCategories', $this->BlogCategories(), $config));
 
         return $fields;
     }

d4030bc80050443f1e63d4f030f7855cfa183b14
Julian Bennecker <jb@mainformat.de>
2019-02-15 15:58:54 +0100

    sort categories by title

--- code/BlogCategory.php
+++ code/BlogCategory.php
@@ -13,6 +13,8 @@ class BlogCategory extends DataObject
         "URLSegment" => "Varchar(255)",
     ];
 
+    private static $default_sort = 'Title';
+
     private static $indexes = [
         "URLSegment" => true,
     ];
